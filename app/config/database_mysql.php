2cbe7f72e3abfb15661a868430a3cafc987a7722
Jon Fredelius <jonfredelius@gmail.com>
2015-11-20 15:45:45 +0100

    removed connection details

--- app/config/database_mysql.php
+++ app/config/database_mysql.php
@@ -12,15 +12,6 @@ return [
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
     //'table_prefix'    => "",
     
-    /*
-    // BTH
-    'dsn'             => "mysql:host=blu-ray.student.bth.se;dbname=jofc13;",
-    'username'        => "jofc13",
-    'password'        => "y{O(4A%d",
-    'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-    'table_prefix'    => "phpmvc_project_",
-    
-    */
     // Display details on what happens
     // 'verbose' => true,
 
